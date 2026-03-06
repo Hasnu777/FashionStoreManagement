e3057355766b77fdbd613d0444d236170961c5fb
Hasnu777 <hassanashah10@gmail.com>
2026-03-06 16:21:48 -0500

    Adding error-checking for addItem()

--- src/main/java/ca/mcgill/ecse/fashionstoremanagement/controller/ItemController.java
+++ src/main/java/ca/mcgill/ecse/fashionstoremanagement/controller/ItemController.java
@@ -4,11 +4,29 @@ import ca.mcgill.ecse.fashionstoremanagement.model.FashionStoreManagement;
 import ca.mcgill.ecse.fashionstoremanagement.model.Item;
 import ca.mcgill.ecse.fashionstoremanagement.model.SizedItem;
 
+import java.util.Objects;
+
 public class ItemController {
 
 	public static void addItem(String name, double price, int points, String sizeStr, int quantity) {
 	// Get FashionStoreManagement instance to perform operations on it
 	FashionStoreManagement fm = FashionStoreManagementController.getFashionStoreManagement();
+	Item existingItem = Item.getWithName(name);
+	if (existingItem != null) {
+		throw new FashionStoreException("an item called \"" + name + "\" already exists");
+	}
+	if (name.isEmpty()) {
+		throw new FashionStoreException("name is required");
+	}
+	if (price <= 0) {
+		throw new FashionStoreException("price must be positive");
+	}
+	if (points <= 1 || points >= 5) {
+		throw new FashionStoreException("points must be between one and five");
+	}
+	if (quantity < 0) {
+		throw new FashionStoreException("quantity must be non-negative");
+	}
 	// Item must be created prior to a SizedItem
 	Item itemToAdd = new Item(name, price, points, fm);
 	// Create SizedItem, in-line execution of acquiring Size enum
@@ -22,12 +40,14 @@ public class ItemController {
 		itemToUpdate.setPrice(newPrice);
 	}
 
-	public static void updateItemPoints(String name, int newPoints) throws FashionStoreException {
-		throw new RuntimeException("TODO");
+	public static void updateItemPoints(String name, int newPoints) {
+		Item itemToUpdate = Item.getWithName(name);
+		itemToUpdate.setLoyaltyPoints(newPoints);
 	}
 
 	public static void deleteItem(String name) throws FashionStoreException {
-		throw new RuntimeException("TODO");
+		Item itemToDelete = Item.getWithName(name);
+		itemToDelete.delete();
 	}
 
 }
