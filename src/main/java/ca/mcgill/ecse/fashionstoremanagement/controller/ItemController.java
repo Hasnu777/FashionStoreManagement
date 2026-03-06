687e6a4d0725f42bda6dac453b5e234e2c7e7949
Hasnu777 <hassanashah10@gmail.com>
2026-03-06 14:36:25 -0500

    Implementing addItem()

--- src/main/java/ca/mcgill/ecse/fashionstoremanagement/controller/ItemController.java
+++ src/main/java/ca/mcgill/ecse/fashionstoremanagement/controller/ItemController.java
@@ -6,9 +6,15 @@ import ca.mcgill.ecse.fashionstoremanagement.model.SizedItem;
 
 public class ItemController {
 
-	public static void addItem(String name, double price, int points, String sizeStr, int quantity)
-			throws FashionStoreException {
-		throw new RuntimeException("TODO");
+	public static void addItem(String name, double price, int points, String sizeStr, int quantity) {
+	// Get FashionStoreManagement instance to perform operations on it
+	FashionStoreManagement fm = FashionStoreManagementController.getFashionStoreManagement();
+	// Item must be created prior to a SizedItem
+	Item itemToAdd = new Item(name, price, points, fm);
+	// Create SizedItem, in-line execution of acquiring Size enum
+	SizedItem sizedItemToAdd = fm.addSizedItem(SizedItem.Size.valueOf(sizeStr), quantity, itemToAdd);
+	// Insert new SizedItem object into FashionStoreManagement instance
+		fm.addSizedItem(sizedItemToAdd);
 	}
 
 	public static void updateItemPrice(String name, double newPrice) throws FashionStoreException {
