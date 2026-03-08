d72e3634bd0a2ec492a528b9f71e542e2e313de2
matt-yst <matthew.ys.tan@gmail.com>
2026-03-07 23:42:53 -0500

    initial code, hopefully no errors

--- src/main/java/ca/mcgill/ecse/fashionstoremanagement/controller/ShipmentController.java
+++ src/main/java/ca/mcgill/ecse/fashionstoremanagement/controller/ShipmentController.java
@@ -12,34 +12,85 @@ import java.util.List;
 public class ShipmentController {
 
     public static int createShipment() throws FashionStoreException {
-        throw new RuntimeException("TODO");
+        // throw new RuntimeException("TODO");
+        try {
+            Shipment s = new Shipment(null, null, null);
+            return s.getShipmentNumber();
+        } catch (Exception e) {
+            throw new FashionStoreException("Could not create shipment");
+        }
+
     }
 
     public static void deleteShipment(int shipmentNumber) throws FashionStoreException {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
+        try {
+            getShipment(shipmentNumber).delete();
+        } catch (Exception e){
+            throw new FashionStoreException("No shipment with this number exists");
+        }
     }
 
     public static void orderShipment(int shipmentNumber, Date dateOrdered) throws FashionStoreException {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
+        try{
+            getShipment(shipmentNumber).setDateOrdered(dateOrdered);
+        } catch (Exception e){
+            throw new FashionStoreException("Unable to order shipment");
+        }
+
     }
 
     public static Shipment getShipment(int shipmentNumber) {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
+            FashionStoreManagement sys = FashionStoreManagementController.getFashionStoreManagement();
+            return sys.getShipment(shipmentNumber);
     }
 
     public static List<Shipment> getAllShipments() {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
+        FashionStoreManagement sys = FashionStoreManagementController.getFashionStoreManagement();
+        return sys.getShipments();
     }
 
     public static List<ShipmentItem> getShipmentItems(int shipmentNumber) {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
+        FashionStoreManagement sys = FashionStoreManagementController.getFashionStoreManagement();
+        return sys.getShipmentItems();
     }
 
     public static void addSizedItemToShipment(int shipmentNumber, String itemName, String sizeStr) throws FashionStoreException {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
+        try {
+            Shipment ship = getShipment(shipmentNumber);
+            Item existingItem = Item.getWithName(itemName);
+            existingItem.addSizedItem(SizedItem.Size.valueOf(sizeStr), 1, existingItem.getFashionStoreManagement());
+        } catch (Exception e){
+            throw new FashionStoreException("Unable to add sized item");
+        }
     }
 
     public static void updateQuantityInShipment(int shipmentNumber, String itemName, String sizeStr, int newQuantity) throws FashionStoreException {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
+        try {
+            Shipment ship = getShipment(shipmentNumber);
+            List <ShipmentItem> l = ship.getShipmentItems();
+
+            for (ShipmentItem si : ship.getShipmentItems()) {
+
+                SizedItem sizedItem = si.getItem();
+                Item item = sizedItem.getItem();
+
+                if (item.getName().equals(itemName) &&
+                        sizedItem.getSize().toString().equals(sizeStr)) {
+
+                    si.setQuantity(newQuantity);
+                    return;
+                }
+            }
+
+        } catch (Exception e){
+            throw new FashionStoreException("Unable to add sized item");
+        }
     }
 }
