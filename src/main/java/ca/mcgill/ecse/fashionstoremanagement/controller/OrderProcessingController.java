8de45650d3b4a816a6a623f58f32066255f2c512
matt-yst <matthew.ys.tan@gmail.com>
2026-03-29 08:42:49 -0400

    Implemented, needs to be debuged & formatted

--- src/main/java/ca/mcgill/ecse/fashionstoremanagement/controller/OrderProcessingController.java
+++ src/main/java/ca/mcgill/ecse/fashionstoremanagement/controller/OrderProcessingController.java
@@ -1,5 +1,13 @@
 package ca.mcgill.ecse.fashionstoremanagement.controller;
 
+import ca.mcgill.ecse.fashionstoremanagement.model.Employee;
+import ca.mcgill.ecse.fashionstoremanagement.model.Order;
+import ca.mcgill.ecse.fashionstoremanagement.model.User;
+import ca.mcgill.ecse.fashionstoremanagement.model.UserRole;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class OrderProcessingController {
 
     public static void checkOut(int orderNumber) {
@@ -13,23 +21,72 @@ public class OrderProcessingController {
     }
 
     public static void assignOrderToEmployee(int orderNumber, String employeeUsername) {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
+
+        //The findEmployee method in the userController class is private for some reason. So I basically recreated the method.
+        Order order = OrderController.getOrder(orderNumber);
+        User user = User.getWithUsername(employeeUsername);
+        if (user == null) {
+            throw new FashionStoreException(
+                    String.format("there is no user with username \"%s\"", employeeUsername));
+        }
+
+        Employee employee = null;
+
+        for (UserRole r : user.getRoles()) {
+            if (r instanceof Employee) {
+               employee = (Employee) r;
+            }
+        }
+        order.assignEmployee(employee);
     }
 
     public static void finishOrderAssembly(int orderNumber) {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
+        Order order = OrderController.getOrder(orderNumber);
+        boolean flag = order.finishAssembly();
+        if (!flag){
+            throw new FashionStoreException("Unable to finish assembly");
+        }
     }
 
     public static void deliverOrder(int orderNumber) {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
         /* Hassan - you must calculate the order's actual delivery deadline in Date form
          by using date placed and deadline variables in the form of a Date (lowkey ask Claude)
          then pass it into the event. The event guard will be checking if this delivery deadline
          is on or before the current date.
          */
+        Order order = OrderController.getOrder(orderNumber);
+        Date datePlaced = order.getDatePlaced();
+        Order.DeliveryDeadline deadline = order.getDeadline();
+        int count;
+        switch (deadline) {
+            case SameDay:
+                count = 0;
+            case InOneDay:
+                count = 1;
+            case InTwoDays:
+                count = 2;
+            case InThreeDays:
+                count = 3;
+            default:
+                throw new FashionStoreException("Invalid deadline");
+        }
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(datePlaced);
+        cal.add(Calendar.DAY_OF_MONTH, count);
+        Date actualDate = cal.getTime();
+        //not sure what to do with the retuned boolean value here
+        order.deliver(actualDate);
     }
 
     public static void cancelOrder(int orderNumber) {
-        throw new RuntimeException("TODO");
+        //throw new RuntimeException("TODO");
+        Order order = OrderController.getOrder(orderNumber);
+        boolean flag = order.cancelOrder();
+        if (!flag){
+            throw new FashionStoreException("Unable to finish assembly");
+        }
     }
 }
--- src/test/java/ca/mcgill/ecse/fashionstoremanagement/feature/OrderProcessingStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/fashionstoremanagement/feature/OrderProcessingStepDefinitions.java
@@ -1,12 +1,25 @@
 package ca.mcgill.ecse.fashionstoremanagement.feature;
 
+import ca.mcgill.ecse.fashionstoremanagement.controller.FashionStoreException;
+import ca.mcgill.ecse.fashionstoremanagement.controller.OrderController;
+import ca.mcgill.ecse.fashionstoremanagement.controller.OrderProcessingController;
+import ca.mcgill.ecse.fashionstoremanagement.controller.UserController;
+import ca.mcgill.ecse.fashionstoremanagement.model.FashionStoreManagement;
+import ca.mcgill.ecse.fashionstoremanagement.model.Order;
 import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class OrderProcessingStepDefinitions extends StepDefinitions {
 
     private int currentOrderNumber;
+
+    private Map<String, Integer> orderIdToNumber = new HashMap<>();
 
     @When("the user attempts to check out the order with ID {string}")
     public void the_user_attempts_to_check_out_the_order_with_id(String orderId) {
@@ -24,25 +37,42 @@ public class OrderProcessingStepDefinitions extends StepDefinitions {
     @When("the manager attempts to assign the order with ID {string} to {string}")
     public void the_manager_attempts_to_assign_the_order_with_id_to(String orderId, String employeeUsername) {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        //TODO
+        try{
+            OrderProcessingController.assignOrderToEmployee(orderIdToNumber.get(orderId), employeeUsername);
+        } catch (FashionStoreException e){
+            StepDefinitions.error = e;
+        }
+        //throw new PendingException();
     }
 
     @When("the user attempts to indicate that assembly of the order with ID {string} is finished")
     public void the_user_attempts_to_indicate_that_assembly_of_the_order_with_id_is_finished(String orderId) {
         // Write code here that turns the phrase above into concrete actions
+        try{
+            OrderProcessingController.finishOrderAssembly(orderIdToNumber.get(orderId));
+        } catch (FashionStoreException e) {
+            StepDefinitions.error = e;
+        }
         throw new PendingException();
     }
 
     @When("the user attempts to cancel the order with ID {string}")
     public void the_user_attempts_to_cancel_the_order_with_id(String orderId) {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        try{
+            OrderProcessingController.cancelOrder(orderIdToNumber.get(orderId));
+        } catch (FashionStoreException e){
+            StepDefinitions.error = e;
+        }
     }
 
     @When("the manager attempts to mark the order with ID {string} as delivered")
     public void the_manager_attempts_to_mark_the_order_with_id_as_delivered(String orderId) {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+        Order.State state = OrderController.getOrder(orderIdToNumber.get(orderId)).getState();
+        assertEquals(state, "delivered"); //prolly not correct implementation
     }
 
     @Then("the order shall be {string}")
@@ -60,7 +90,9 @@ public class OrderProcessingStepDefinitions extends StepDefinitions {
     @Then("the order's assignee shall be {string}")
     public void the_order_s_assignee_shall_be(String employeeUsername) {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Order order = OrderController.getOrder(currentOrderNumber);
+        assert order != null;
+        assertEquals(order.getOrderAssignee().getUser().getUsername(), employeeUsername);
     }
 
     @Then("the order's date placed shall be today")
